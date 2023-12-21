48a5209072a2dbca6779e2a2f3914d739c8c425f
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-21 11:09:18 +0100

    Added a POST route

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -4,8 +4,19 @@ import Product from "../../../db/models/Product";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const products = await Product.find();
-    return response.status(200).json(products);
+  try {
+    if (request.method === "GET") {
+      const products = await Product.find();
+      return response.status(200).json(products);
+    }
+
+    if (request.method === "POST") {
+      const productData = request.body;
+      await Product.create(productData);
+      return response.status(201).json({ status: "Product created" });
+    }
+  } catch (error) {
+    console.error(error);
+    return response.status(400).json({ error: error.message });
   }
 }
